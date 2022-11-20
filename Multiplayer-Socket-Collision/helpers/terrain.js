770fca7d1ac90a675aef96c9b4822c39bd87eed3
aka-blackboots <47943405+aka-blackboots@users.noreply.github.com>
2022-11-21 01:19:55 +0530

    Multiplayer-Game

---
+++ Multiplayer-Socket-Collision/helpers/terrain.js
@@ -0,0 +1,64 @@
+import {
+    DoubleSide,
+    Mesh,
+    MeshBasicMaterial,
+    PlaneGeometry,
+    Scene,
+    TextureLoader,
+    MeshPhongMaterial,
+    RepeatWrapping,
+    sRGBEncoding,
+    AmbientLight,
+    DirectionalLight,
+    Fog,
+    Color
+} from 'three';
+
+
+export class Terrain {
+    constructor(scene) {
+        this.scene = scene;
+
+        this.background();
+        this.basicLights();
+        this.createBasicTerrain();
+    }
+
+    background(){
+        this.scene.background = new Color(0xffffff);
+        //this.scene.fog = new Fog(0xffffff, 10, 200);
+    }
+
+    basicLights(){
+        this.scene.add(new AmbientLight(0x222222));
+
+        const dLight = new DirectionalLight(0xffffff, 2.25);
+        //dLight.position.set(200,450,500);
+        dLight.castShadow = true;
+
+        this.scene.add( dLight );
+    }
+
+    createBasicTerrain() {
+
+        const groundTerrain = new TextureLoader().load('https://i.postimg.cc/VvNZKKnB/grasslight-big.jpg');
+        const groundGeometry = new PlaneGeometry(1000, 1000);
+        const groundMaterial = new MeshPhongMaterial({
+            color: 0xffffff,
+            map: groundTerrain
+        });
+
+        //const groundMaterial = new MeshBasicMaterial({color: 'green', side: DoubleSide});
+
+        const ground = new Mesh(groundGeometry, groundMaterial);
+        ground.rotation.x = - Math.PI/2;
+        ground.material.map.repeat.set( 512, 512 );
+        ground.material.map.wrapS = RepeatWrapping;
+        ground.material.map.wrapT = RepeatWrapping;
+        ground.material.map.encoding = sRGBEncoding;
+        //ground.receiveShadow = true;
+        this.scene.add(ground)
+    }
+
+
+}
