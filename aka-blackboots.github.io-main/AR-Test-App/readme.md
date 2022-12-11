a4561a5e3a7a5bf65246921b714ec43124a54503
Vishwajeet Mane <manevishwajeet1@gmail.com>
2022-12-11 11:15:51 +0530

    AR-Demo

---
+++ aka-blackboots.github.io-main/AR-Test-App/readme.md
@@ -0,0 +1,12 @@
+# How to run locally
+- Clone the repo
+- Open in VS Code
+- Using Live Server Extension run the index.html file
+- In Browser install "CORS" extension as our videos will be loaded using external server
+
+# Features Added
+- Running the external Video
+- ThreeJS Scene to Show the Concert Environment
+
+# TODO
+-
---
+++ aka-blackboots.github.io-main/AR-Test-App/src/app.js
@@ -0,0 +1,90 @@
+import {
+  AmbientLight,
+  AxesHelper,
+  BoxGeometry,
+  DirectionalLight,
+  DoubleSide,
+  FrontSide,
+  GridHelper,
+  Mesh,
+  MeshBasicMaterial,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Scene,
+  WebGLRenderer
+} from "three";
+import {
+  OrbitControls
+} from "three/examples/jsm/controls/OrbitControls.js";
+
+// External Packages
+
+const canvas = document.getElementById("renderCanvas");
+const playBtn = document.getElementById("playBtn");
+
+const scene = new Scene();
+let camera, renderer, controls;
+
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+function init() {
+  //Creates the camera (point of view of the user)
+  const aspect = size.width / size.height;
+  camera = new PerspectiveCamera(75, aspect);
+  camera.position.x = 0;
+  camera.position.y = 1.7;
+  camera.position.z = -2;
+  camera.lookAt(0, 0.9, 0)
+
+  const ambientLight = new AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(0, 10, 0);
+  directionalLight.target.position.set(-5, 0, 0);
+  scene.add(directionalLight);
+  scene.add(directionalLight.target);
+
+  renderer = new WebGLRenderer({
+    antialias: true,
+    alpha: true,
+    canvas: canvas
+  });
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setClearColor(0xc9c9c9);
+  
+  initOrbitControls();
+}
+
+function initOrbitControls(){
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
+  controls.target.set(0, 1, 0);
+}
+
+//Animation loop
+const animate = () => {
+
+  renderer.render(scene, camera);
+  controls.update;
+
+  requestAnimationFrame(animate);
+};
+
+init();
+animate();
+
+//Adjust the viewport to the size of the browser
+window.addEventListener("resize", () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(size.width, size.height);
+});
